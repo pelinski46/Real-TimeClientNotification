9a5b3a7e8db53ac5a3e390945b000bfbdb707760
Pablo <pelinski46@gmail.com>
2024-06-20 16:00:23 -0300

    Add project files.

---
+++ Client/SignalConnections/SignalConnection.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Client.SignalConnections
+{
+    public class SignalConnection(NavigationManager NavManager)
+    {
+        public event Action? ConnectionStateChanged;
+        public string ConnectionSate = string.Empty;
+        public readonly HubConnection hubConnection = new HubConnectionBuilder()
+            .WithUrl(NavManager.ToAbsoluteUri("https://localhost:7082/connect"))
+            .Build();
+        public async Task StartConnection()
+        {
+            if (hubConnection.State != HubConnectionState.Connected)
+            {
+                await hubConnection.StartAsync();
+            }
+            GetConnectionState();
+        }
+
+        public async Task CloseConnection()
+        {
+            if (hubConnection.State == HubConnectionState.Connected)
+            {
+                await hubConnection.StopAsync();
+            }
+            GetConnectionState();
+        }
+
+        public void GetConnectionState()
+
+
+    }
+}
---
+++ Server/SignalRHubs/SignalRConnectionhub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace Server.SignalRHubs
+{
+    public class SignalRConnectionHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        => await Clients
+                 .All
+                 .SendAsync("AllClientsNotification", $"{Context.ConnectionId} has joined {Environment.NewLine} say 'Hi' to him");
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await Clients
+                  .All
+                  .SendAsync("AllClientsNotification", $"{Context.ConnectionId} just left");
+        }
+
+    }
+}
